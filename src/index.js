a81c0b9bea2ef461bd30702615b081537e06c6b4
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-15 19:36:48 +0530

    update : 04-IMG asset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import App from './App'
+import Logo from './logo.svg'
 const root = document.getElementById('root');
 root.innerHTML = `
+<img src="${Logo}" alt="build icon" />
 <h1>learning web tooling</h1>
 ${App({ name: " Tanay" })}
 `
