5d1b3bda0a3621f7622a616eb5ba6cc2e04206c2
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-15 19:16:47 +0530

    update : 02-babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,21 @@ const webpackConfig = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    }, module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
     },
+
 
     mode: "production"
 };
